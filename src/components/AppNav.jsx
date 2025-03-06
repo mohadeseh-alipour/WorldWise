18b9c42c906364d0177ae7708282e89c3a9adce0
mohadeseh <mohadeseh.alipour27@gmail.com>
2025-03-06 19:27:12 +0100

    Create Pages and components and module.css

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./AppNav.module.css";
+
+const AppNav = () => {
+  return <nav className={styles.nav}>App navigation</nav>;
+};
+
+export default AppNav;
