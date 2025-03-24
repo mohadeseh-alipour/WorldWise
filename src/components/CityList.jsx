ac1f3126ebfee69b7e46395ab7d2991df6c75305
mohadeseh <mohadeseh.alipour27@gmail.com>
2025-03-24 22:38:35 +0100

    Refactoring the code to use Context

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -3,9 +3,13 @@ import Spinner from "./Spinner";
 import styles from "./CityList.module.css";
 import CityItems from "./CityItems";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-const CityList = ({ cities, isLoading }) => {
+const CityList = () => {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
+
   if (!cities.length)
     return (
       <Message message="Add your first city by clicking on a city on the map" />
